e90631e699047e93022770fcf5175b6ffff07280
ilan <supox0@walla.co.il>
2018-06-08 23:03:01 +0300

    Setting default timezone to be Jerusalem

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,6 @@ module IstraBet
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.time_zone = 'Asia/Jerusalem'
   end
 end
