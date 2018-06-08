ed8e18ac6be0b89e567e7eb90ca3d95fd00a82c2
ilan <supox0@walla.co.il>
2018-06-08 14:01:37 +0300

    devise: add confirmable

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,11 @@ class User < ApplicationRecord
   validates :name, length: { in: 2..100 }
   validates_uniqueness_of :name
   validates_format_of :name, with: /^[a-zA-Z0-9_\. ]*$/, :multiline => true
-
-
+  after_create :skip_conf!
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable #, :confirmable
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
 
   def to_s
     self.name
@@ -18,4 +17,11 @@ class User < ApplicationRecord
   def score
     bets.sum(&:points)
   end
+
+private
+
+  def skip_conf!
+    self.confirm if Rails.env.development?
+  end
+
 end
