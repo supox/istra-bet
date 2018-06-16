abb44319b4dcc142ad93d0247b772946b7545019
ilan <supox0@walla.co.il>
2018-06-16 20:24:04 +0300

    Added preview mail

---
+++ spec/mailers/previews/rounds_mailer_preview.rb
@@ -0,0 +1,7 @@
+# Preview all emails at http://localhost:3000/rails/mailers/example_mailer
+class RoundsMailerPreview < ActionMailer::Preview
+  def notify_round_preview
+    RoundsMailer.notify_round(text="hello guys\nlets party!!", subject="test subject", round=Round.first, users=User.all)
+  end
+end
+
